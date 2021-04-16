5f686cd05f90c883238fee56ee1e32800e501191
sanvas <sanvas@bancolombia.com.co>
2021-04-16 09:49:47 -0500

    Creacion de promesas, trabajo con fechas y funciones matematicas

---
+++ holaMundo/scripts/33-TryCatch.js
@@ -0,0 +1,13 @@
+'use strict'
+
+window.addEventListener('load', function() {
+
+    try {
+        var year = 2019;
+        alert(year);
+        console.log(decodeURIComponent("https://vifjreocnjo##$$%r.es"))
+    } catch (error) {
+        alert(`Ocurrio el error ${error} al momento de cargar`);
+    }
+
+})
