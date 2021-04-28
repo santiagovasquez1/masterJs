6b8d926f45a01371aff260ee4eaf9358c393f3a4
svasquez <svasquez@controldiseno.com>
2021-04-28 08:03:16 -0500

    Creacion de peticiones http y configuracion de index

---
+++ backend_NodeJs/app.js
@@ -0,0 +1,44 @@
+//Archivo encargado de las peticiones htpp
+'use strict'
+var express = require('express');
+var bodyParser = require('body-parser');
+const { response, request } = require('express');
+
+var app = express();
+
+//cargar archivos de rutas
+
+
+//middleware, metodos que se ejecuta antes de la accion de un controlador
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//Cors
+
+
+
+//rutas
+app.get('/test', (request, response) => {
+    response.status(200).send({
+        message: 'Hola mundo desde el backend'
+    });
+});
+
+app.get('/', (request, response) => {
+    var plantilla = `<h1>Pagina de inicio</h1>`
+    response.status(200).send(plantilla);
+});
+
+app.post('/test/:id', (request, response) => {
+    console.log(request.body.name);
+    console.log(request.query.web);
+    console.log(request.params.id);
+    response.status(200).send({
+        message: 'Hola mundo desde el backend',
+        body: request.body
+    });
+});
+
+//exportar modulo
+module.exports = app;
--- backend_NodeJs/index.js
+++ backend_NodeJs/index.js
@@ -1,1 +1,22 @@
 //Fichero principal, para la configuracion del backend
+'use strcit'
+
+const { response } = require('express');
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3700;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/PortafolioDb')
+    .then(result => {
+        console.log('Conexión establecida con exito');
+
+        //Creacion del servidor
+        app.listen(port, () => {
+            console.log('Servidor corriendo correctamente en url: localhost:3700');
+        });
+
+
+    }).catch(error => {
+        console.log(error);
+    });
