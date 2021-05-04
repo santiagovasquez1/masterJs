ece48ced2afb2dad333bb2468ffebc2cea189549
svasquez <svasquez@controldiseno.com>
2021-05-04 07:34:56 -0500

    itegracion de connect multiparty

--- backend_TypeScript/src/app.ts
+++ backend_TypeScript/src/app.ts
@@ -1,32 +1,33 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
- 
+
 export class App {
-  public app: express.Application;
-  public port: number;
- 
-  constructor(controllers, port) {
-    this.app = express();
-    this.port = port;
- 
-    this.initializeMiddlewares();
-    this.initializeControllers(controllers);
-  }
- 
-  private initializeMiddlewares() {
-    this.app.use(bodyParser.json());
-  }
- 
-  private initializeControllers(controllers) {
-    controllers.forEach((controller) => {
-      this.app.use('/api', controller.router);
-    });
-  }
- 
-  public listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
-    });
-  }
+    public app: express.Application;
+    public port: number;
+
+
+    constructor(controllers, port) {
+        this.app = express();
+        this.port = port;
+
+
+        this.initializeMiddlewares();
+        this.initializeControllers(controllers);
+    }
+
+    private initializeMiddlewares() {
+        this.app.use(bodyParser.json());
+    }
+
+    private initializeControllers(controllers) {
+        controllers.forEach((controller) => {
+            this.app.use('/api', controller.router);
+        });
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the port ${this.port}`);
+        });
+    }
 }
- 
--- backend_TypeScript/src/controllers/projectController.ts
+++ backend_TypeScript/src/controllers/projectController.ts
@@ -6,13 +6,20 @@ export class ProjectController {
     proectModel: projectModels;
     public path = 'api';
     public router = express.Router();
+    public multpart: any;
+    public multipartMiddelware;
 
     constructor() {
         this.initalizeRoutes();
+        this.multpart = require('connect-multiparty');
+        this.multipartMiddelware = this.multpart({
+            uploadDir: '/uploads'
+        });
     }
 
     initalizeRoutes() {
         this.router.get('/home', this.home);
+        this.router.post('/upLoadImage/:id', this.multipartMiddelware, this.uploadImage);
     }
 
     home(req: express.Request, res: express.Response) {
@@ -21,4 +28,12 @@ export class ProjectController {
             message: 'Soy la Home',
         });
     }
+
+    uploadImage(req: express.Request, res: express.Response) {
+        return res.status(200).send({
+            headers: req.headers,
+            message: 'Soy upload',
+        });
+    }
+
 }
