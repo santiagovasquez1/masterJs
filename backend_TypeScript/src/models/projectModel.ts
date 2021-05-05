b2e6a34457531cf19b65857fe4b20c4209e29b8d
sanvas <sanvas@bancolombia.com.co>
2021-05-05 10:42:27 -0500

    Se crea metodo para crear un nuevo proyecto en la tabla

--- backend_TypeScript/src/models/projectModel.ts
+++ backend_TypeScript/src/models/projectModel.ts
@@ -2,16 +2,30 @@ import { Guid } from "guid-typescript";
 
 class projectModels {
 
-    private id: String;
-    projectName: String;
-    description: String;
-    category: String;
-    projectYear: Number;
-    lenguajes: String[];
-    image: String;
+    private id: {
+        S: String;
+    };
+    projectName: {
+        S: string
+    };
+    description: {
+        S: string
+    };
+    category: {
+        S: string
+    };
+    projectYear: {
+        N: string
+    };
+    lenguajes: {
+        S: string
+    };
+    image: {
+        S: string
+    };
 
     constructor() {
-        this.id = Guid.create().toString();
+        this.id.S = Guid.create().toString();
     }
 }
 
