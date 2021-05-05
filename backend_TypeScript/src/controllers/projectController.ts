d6bd4d8d528c101e6246e22f0202793db9447dff
sanvas <sanvas@bancolombia.com.co>
2021-05-05 16:36:18 -0500

    Metodos para actualizar, obtener y eliminar proyecto

--- backend_TypeScript/src/controllers/projectController.ts
+++ backend_TypeScript/src/controllers/projectController.ts
@@ -14,6 +14,7 @@ class ProjectController {
     private projectControllerLogger: eventLog = eventLogSingleton;
     private projects: projectModels[] = [];
     private awsDynamoDbConnection: AwsDynamoDbConnection = AwsDynamoDbConnectionSingleton;
+    private readonly tableName = "projects";
 
     constructor() {
         this.projectControllerLogger.log('projectController instance constructed');
@@ -26,7 +27,10 @@ class ProjectController {
 
     initalizeRoutes() {
         this.router.get('/home', this.home.bind(this));
+        this.router.get('/get', this.getProjectById.bind(this));
         this.router.post('/saveProject', this.saveProject.bind(this));
+        this.router.put('/updateProject', this.uploadProject.bind(this));
+        this.router.delete('/deleteProject', this.deleteProject.bind(this));
         // this.router.post('/upLoadImage/:id', this.multipartMiddelware, this.uploadImage.bind(this));
     }
 
@@ -34,9 +38,8 @@ class ProjectController {
 
         this.projectControllerLogger.log('home has been invoked sucessfull');
         let params = {
-            TableName: "projects"
+            TableName: this.tableName
         };
-
 
         this.awsDynamoDbConnection.awsDynamoDb.scan(params, (err, data) => {
             if (err) {
@@ -62,6 +65,47 @@ class ProjectController {
         });
     }
 
+    getProjectById(req: express.Request, res: express.Response) {
+        let projectId = req.params.id;
+
+        if (projectId == null || projectId == undefined) {
+            return res.status(500).send({
+                message: "Error al cargar projecto",
+                err: "projectId no puede ser nulo"
+            });
+        }
+
+        let params = {
+            FilterExpression: "id = :i",
+            ExpressionAttributeValues: {
+                ":i": { S: projectId }
+            },
+            TableName: this.tableName
+        };
+
+        this.projectControllerLogger.log("Ejecutando getProjectById");
+        this.awsDynamoDbConnection.awsDynamoDb.scan(params, (err, projectResult) => {
+            if (err) {
+                this.projectControllerLogger.log(err.stack);
+                return res.status(500).send({
+                    message: "Error al crear projecto",
+                    error: err
+                });
+            }
+            if (!projectResult) {
+                this.projectControllerLogger.log("No se ha obtenido ningun objeto");
+                return res.status(404).send({
+                    message: "No se ha obtenido ningun objeto"
+                });
+            }
+
+            this.projectControllerLogger.log("Datos cargados correctamente");
+            return res.status(200).send({
+                project: projectResult.Items[0] as unknown as projectModels
+            });
+        });
+    }
+
     saveProject(req: express.Request, res: express.Response) {
 
         let params = req.body;
@@ -77,7 +121,7 @@ class ProjectController {
             TableName: "projects",
             Item: project
         }
-        this.projectControllerLogger.log("Iniciando la carga en BD");
+        this.projectControllerLogger.log("Ejecutando  saveProject");
         this.awsDynamoDbConnection.awsDynamoDb.putItem(params, (err, data) => {
             if (err) {
                 this.projectControllerLogger.log(err.stack);
@@ -92,18 +136,93 @@ class ProjectController {
                 });
             }
             this.projectControllerLogger.log("Datos cargados correctamente");
-            console.log(data)
             return res.status(200).send({
-                projects: data as unknown as projectModels
+                projects: project
             });
         });
+    }
 
+    uploadProject(req: express.Request, res: express.Response) {
+
+        let projectId = req.params.id;
+        let update = req.body;
+
+        let params = {
+            TableName: this.tableName,
+            Key: {
+                "id": {
+                    S: projectId
+                }
+            },
+            UpdateExpression: "set projectName = :n, description = :d, category = :c, projectYear = :y, lenguajes = :l, image = :i ",
+            ExpressionAttributeValues: {
+                ":n": { S: update.projectName },
+                ":d": { S: update.description },
+                ":c": { S: update.category },
+                ":y": { N: update.projectYear.toString() },
+                ":l": { S: update.langs },
+                ":i": { S: update.image }
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+        this.updateDataBase(params, res);
+    }
+
+    deleteProject(req: express.Request, res: express.Response) {
+
+        let projectId = req.params.id;
+
+        let params = {
+            TableName: this.tableName,
+            Key: {
+                "id": {
+                    S: projectId
+                }
+            },
+        };
+
+        this.awsDynamoDbConnection.awsDynamoDb.deleteItem(params, (err, result) => {
+            if (err) {
+                return res.status(500).send({
+                    message: "Error al eliminar el proyecto",
+                    error: err
+                });
+            }
+            if (!result) {
+                return res.status(404).send({
+                    message: "No existe el poyecto para eliminar",
+                });
+            }
+            return res.status(200).send({
+                project: result
+            });
+        });
     }
 
     uploadImage(req: express.Request, res: express.Response) {
         return res.status(200).send({
             headers: req.headers,
             message: 'Soy upload',
+        });
+    }
+
+    updateDataBase(params, res: express.Response) {
+        this.awsDynamoDbConnection.awsDynamoDb.updateItem(params, (err, projectUpdate) => {
+            if (err) {
+                return res.status(500).send({
+                    message: "Error al actualizar el proyecto",
+                    error: err
+                });
+            }
+            if (!projectUpdate) {
+                return res.status(404).send({
+                    message: "No existe el poyecto para actualizar",
+                });
+            }
+            return res.status(200).send({
+                project: projectUpdate as unknown as projectModels
+            });
         });
     }
 
